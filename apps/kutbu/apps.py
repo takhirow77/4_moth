0f4f07bafb75d2ac1f9dcfa0748c8dd615c4f8b6
takhirow77 <tahirows77@icloud.com>
2024-04-20 15:39:24 +0300

    first commit

---
+++ apps/kutbu/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KutbuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.kutbu'
---
+++ apps/nurbo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NurboConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.nurbo'
